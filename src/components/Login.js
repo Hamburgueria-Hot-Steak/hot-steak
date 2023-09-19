72d470701349a811106d4445c995571b156252c2
Maria de Fátima Nunes Alves <94319702+alvesmariadefatima@users.noreply.github.com>
2023-09-19 17:22:02 -0300

    Initial Commit 🎉

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,7 @@ import './Login.css';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+
   return (
     <div>
       <Link to="/Home"> {/* Define o destino da rota para a página inicial */}
@@ -13,11 +14,9 @@ const Login = () => {
         <div className="login-form">
           <p className="txt-login">FAÇA SEU LOGIN</p>
           <div className="form-email">
-            <input type="email" id="email" className="red-input-filled" />
-            </div>
-            <div className="form-control">
-            <input type="password" id="senha" className="red-input-filled" />
-            </div>
+            <input type="text" placeholder="Email" className="red-input-filled" onfocus="clearInput(this)" />
+            <input type="password" placeholder="Senha" className="red-input-filled" onfocus="clearInput(this)" maxlength="8" />
+          </div>
           <button className="login-button">Entrar</button>
           <div className="links">
             <a href="">Esqueci minha senha</a>
