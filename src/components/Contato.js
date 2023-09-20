265716d6e312da95998677f5b9af0faf41fbf37d
Maria de Fátima Nunes Alves <94319702+alvesmariadefatima@users.noreply.github.com>
2023-09-20 15:33:25 -0300

    Delete src directory

--- src/components/Contato.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Contato= () => {
-    return (
-        <h1>Contato</h1>
-    )
-}
-
-export default Contato;
--- src/components/Delivery.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Delivery = () => {
-    return (
-        <h1>Delivery</h1>
-    )
-}
-
-export default Delivery;
