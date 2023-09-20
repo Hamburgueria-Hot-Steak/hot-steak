b477230d39d4ee8d20dae0786d9c4ca59bb8435f
Maria de Fátima Nunes Alves <94319702+alvesmariadefatima@users.noreply.github.com>
2023-09-20 15:33:55 -0300

    Initial Commit 🎉

---
+++ src/components/Login.js
@@ -0,0 +1,121 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './Login.css';
+import { Link } from 'react-router-dom';
+import Loading from '../components/Loading';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showWarning, setShowWarning] = useState(false);
+  const emailInputRef = useRef(null);
+  const passwordInputRef = useRef(null);
+  const loginButtonRef = useRef(null);
+  const forgotPasswordLinkRef = useRef(null);
+  const createAccountLinkRef = useRef(null);
+
+  const handleLoginClick = () => {
+    if (email === '' || password === '') {
+      setShowWarning(true);
+      alert('Preencha os campos!');
+    } else {
+      setShowWarning(false);
+      alert('Login realizado com sucesso!');
+    }
+  };
+
+  const handleTabKeyPress = (e) => {
+    if (e.key === 'Tab') {
+      e.preventDefault();
+      if (document.activeElement === emailInputRef.current) {
+        passwordInputRef.current.focus();
+      } else if (document.activeElement === passwordInputRef.current) {
+        loginButtonRef.current.focus();
+      } else if (document.activeElement === loginButtonRef.current) {
+        forgotPasswordLinkRef.current.focus();
+      } else if (document.activeElement === forgotPasswordLinkRef.current) {
+        createAccountLinkRef.current.focus();
+      } else if (document.activeElement === createAccountLinkRef.current) {
+        emailInputRef.current.focus();
+      }
+    }
+  };
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <div>
+      <Link to="/Home">
+        <div className="logo-container">
+          <img src="./assets/Logo Hot Steak.png" alt="Logotipo Hot Steak" />
+        </div>
+      </Link>
+      <div className="login-container">
+        <div className="login-form">
+          <p className="txt-login">FAÇA SEU LOGIN</p>
+          <div className="form-email">
+            <input
+              type="text"
+              placeholder="Email"
+              className="red-input-filled"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              ref={emailInputRef}
+              onKeyDown={handleTabKeyPress}
+              tabIndex="1" // Ordem de tabulação
+            />
+            <input
+              type="password"
+              placeholder="Senha"
+              className="red-input-filled"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              maxLength="8"
+              ref={passwordInputRef}
+              onKeyDown={handleTabKeyPress}
+              tabIndex="2" // Ordem de tabulação
+            />
+          </div>
+          <button
+            className="login-button"
+            onClick={handleLoginClick}
+            ref={loginButtonRef}
+            onKeyDown={handleTabKeyPress}
+            tabIndex="3" // Ordem de tabulação
+          >
+            Entrar
+          </button>
+          <div className="links">
+            <a
+              href=""
+              ref={forgotPasswordLinkRef}
+              onKeyDown={handleTabKeyPress}
+              tabIndex="4" // Ordem de tabulação
+            >
+              Esqueci minha senha
+            </a>
+            <a
+              href=""
+              ref={createAccountLinkRef}
+              onKeyDown={handleTabKeyPress}
+              tabIndex="5" // Ordem de tabulação
+            >
+              Criar nova conta
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
