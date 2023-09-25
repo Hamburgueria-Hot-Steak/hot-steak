c7e9abbe2653a75521341c3b4043211c9581466d
Maria de Fátima Nunes Alves <94319702+alvesmariadefatima@users.noreply.github.com>
2023-09-25 16:27:32 -0300

    Initial Commit 🎉

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,14 +4,13 @@ import './Navbar.css';
 
 const Navbar = () => {
     return (
-      <nav className="menu-container">
-        <img className="logo-hot-steak" src="./assets/Logo Hot Steak.png" alt="" />
-        <a href="./Home">Home</a>
-        <a href="./Delivery">Delivery</a>
-        <a href="./Sobre">Sobre</a>
-        <a href="./Contato">Contato</a>
-        <a href="./Login">Login</a>
-      </nav>
+      <nav className="menu-container" style={{ justifyContent: "flex-end" }}>
+      <a href="./Home">Home</a>
+      <a href="./Delivery">Delivery</a>
+      <a href="./Sobre">Sobre</a>
+      <a href="./Contato">Contato</a>
+      <a href="./Login">Login</a>
+    </nav>
     );
 }
 
