9e3d76d3f08606fd3a6c03e55adf07ebb882df5b
Maria de Fátima Nunes Alves <94319702+alvesmariadefatima@users.noreply.github.com>
2023-09-26 13:11:28 -0300

    Initial Commit 🎉

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
     return (
       <nav className="menu-container" style={{ justifyContent: "flex-end" }}>
-      <a href="./Home">Home</a>
-      <a href="./Delivery">Delivery</a>
-      <a href="./Sobre">Sobre</a>
-      <a href="./Contato">Contato</a>
-      <a href="./Login">Login</a>
-    </nav>
+          <a href="./Home">Home</a>
+          <a href="./Delivery">Delivery</a>
+          <a href="./Sobre">Sobre</a>
+          <a href="./Contato">Contato</a>
+          <a href="./Login">Login</a>
+      </nav>
     );
 }
 
