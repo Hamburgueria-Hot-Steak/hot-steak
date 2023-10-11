eea449181268e3f66c9fb386547016c85dc2d8ad
Maria de Fátima Nunes Alves <94319702+alvesmariadefatima@users.noreply.github.com>
2023-10-11 14:42:39 -0300

    Initial Commit 🎉

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@ import './Navbar.css';
 const Navbar = () => {
     return (
       <nav className="menu-container" style={{ justifyContent: "flex-end" }}>
+          <a href="./Home"><img className="logotipo-hot-steak-horizontal" src="./assets/Logotipo Hot Steak Horizontal.png" alt="Logotipo Hot Steak Horizontal" /></a>
           <a href="./Home">Home</a>
           <a href="./Delivery">Delivery</a>
           <a href="./Sobre">Sobre</a>
