f750b277bc568c4383a5cae47a618a719ca8c33b
Maria de Fátima Nunes Alves <94319702+alvesmariadefatima@users.noreply.github.com>
2023-10-17 13:39:48 -0300

    Initial Commit 🎉

---
+++ src/Routes.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Home from './components/pages/Home';
+import Delivery from './components/pages/Delivery';
+import Sobre from './components/pages/Sobre';
+import Contato from './components/pages/Contato';
+import Login from './components/pages/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const AppRoutes = () => {
+    return (
+        <Router>
+            <Routes>
+                {/* Rota raiz para o componente Home */}
+                <Route path="/" element={<Home />} />
+
+                {/* Outras rotas */}
+                <Route path="/home" element={<Home />} />
+                <Route path="/delivery" element={<Delivery />} />
+                <Route path="/sobre" element={<Sobre />} />
+                <Route path="/contato" element={<Contato />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default AppRoutes;
