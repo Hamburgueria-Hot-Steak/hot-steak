be761fa732b94d8f979da76475bc326dba7e7412
Maria de Fátima <mnunesalves334@gmail.com>
2023-10-30 12:57:20 -0300

    Ajustes no escopo do código - Remoção do componente Contato

--- src/Routes.js
+++ src/Routes.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Home from './components/pages/Home';
 import Delivery from './components/pages/Delivery';
 import Sobre from './components/pages/Sobre';
-import Contato from './components/pages/Contato';
 import Login from './components/pages/Login';
 import Cadastro from './components/pages/Cadastro';
 import TelaRecuperacaoSenha from './components/pages/TelaRecuperacaoSenha';
@@ -19,7 +18,6 @@ const AppRoutes = () => {
         <Route path="/home" element={<Home />} />
         <Route path="/delivery" element={<Delivery />} />
         <Route path="/sobre" element={<Sobre />} />
-        <Route path="/contato" element={<Contato />} />
         <Route path="/login" element={<Login />} />
         <Route path="/TelaRecuperacaoSenha" element={<TelaRecuperacaoSenha />} />
         <Route path="/TelaRecuperacaoSenhaCodigo" element={<TelaRecuperacaoSenhaCodigo />} />
