a7d729a51074a48f861c9fff652701bb493d3263
Maria de Fátima <mnunesalves334@gmail.com>
2023-11-01 13:33:48 -0300

    Initial Commit 🎉

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -32,38 +32,40 @@ const Home = () => {
       </h2>
       <div className="opcoes-hamburguer-container">
       <div className="combos">
-        <div className="combo combo1">
+        <div className="combo1">
           <img
             className="hamburguer1"
             src="./assets/combo-hamburguer1.jpg"
-            alt="2 Hambúrguer + Batata Frita + Refrigerante"
-            title="2 Hambúrguer + Batata Frita + Refrigerante"
+            alt="2 Hambúrgueres + Batata Frita + Refrigerante"
+            title="2 Hambúrgueres + Batata Frita + Refrigerante"
           />
-          <p>2 Hambúrguer + Batata Frita + Refrigerante</p>
+          <p style={{fontSize: '15px'}}>2 hambúrgueres +</p>
+          <p style={{fontSize: '15px'}}>Batata Frita +</p>
+          <p style={{fontSize: '15px'}}>Refrigerante</p>
         </div>
-        <div className="combo combo2">
+        <div className="combo2">
           <img className="hamburguer2" src="./assets/combo-hamburguer2.jpg" alt="3 Hambúrgueres" />
-          <p>Triple Hotsteak Burguer</p>
+          <p style={{ fontSize: '15px' }}>Triple Hotsteak Burguer</p>
         </div>
 
-        <div className="combo combo3">
+        <div className="combo3">
           <img className="hamburguer3" src="./assets/combo-hamburguer3.jpg" alt="Hambúrguer e Batata Frita" />
-          <p>Hambúrguer + Batata Frita</p>
+          <p style={{ fontSize: '15px' }}>Hambúrguer + Batata Frita</p>
         </div>
 
-        <div className="combo combo4">
+        <div className="combo4">
           <img className="hamburguer4" src="./assets/combo-hamburguer4.jpg" alt="Combo Infantil" />
-          <p>Combo infantil</p>
+          <p style={{ fontSize: '15px' }}>Hot Junior + Batata Frita</p>
         </div>
 
-        <div className="combo combo5">
+        <div className="combo5">
           <img className="hamburguer5" src="./assets/combo-hamburguer5.jpg" alt="Hambúrguer de frango + Batata Frita" />
-          <p>Hambúrguer de frango + Batata Frita</p>
+          <p style={{ fontSize: '15px' }}>Hot Costela + Batata Frita</p>
         </div>
 
-        <div className="combo combo6">
+        <div className="combo6">
           <img className="hamburguer6" src="./assets/combo-hamburguer6.png" alt="Combo vegano" />
-          <p>Combo vegano</p>
+          <p style={{ fontSize: '15px' }}>Hambúrguer + Batata Frita</p>
         </div>
       </div>
     </div>
@@ -72,34 +74,34 @@ const Home = () => {
         <div className="grupo-1">
           <div className="opcao-hamburguer1">
             <img src="./assets/Hamburguer-1.webp" alt="Hamburguer Especial Hot Steak" />
-            <h1 style={{ fontSize: '20px' }}>Especial Hot Steak</h1>
+            <h1 style={{ fontSize: '15px' }}>Especial Hot Steak</h1>
           </div>
 
           <div className="opcao-hamburguer2">
             <img src="./assets/opcao-cheddar-bacon-hamburgueres.png" alt="Cheddar e Bacon" />
-            <h1 style={{ fontSize: '20px' }}>Cheddar e Bacon</h1>
+            <h1 style={{ fontSize: '15px' }}>Cheddar e Bacon</h1>
           </div>
 
           <div className="opcao-hamburguer3">
             <img src="./assets/opcao-hamburguer-mega.webp" alt="Hamburguer Mega" />
-            <h1 style={{ fontSize: '20px' }}>Mega</h1>
+            <h1 style={{ fontSize: '15px' }}>Mega</h1>
           </div>
         </div>
 
         <div className="grupo-2">
           <div className="opcao-hamburguer4">
             <img src="./assets/opcao-hotfrango-hamburgueres.png" alt="Hot Frango" />
-            <h1>HotFrango</h1>
+            <h1 style={{ fontSize: '15px' }}>HotFrango</h1>
           </div>
 
           <div className="opcao-hamburguer5">
             <img src="./assets/opcao-black-burguer-hamburgueres.jpg" alt="Black Burguer" />
-            <h1>Black Burguer</h1>
+            <h1 style={{ fontSize: '15px' }}>Black Burguer</h1>
           </div>
 
           <div className="opcao-hamburguer6">
             <img src="./assets/opcao-x-tudo.jpeg" alt="X-Tudo" />
-            <h1>X-Tudo</h1>
+            <h1 style={{ fontSize: '15px' }}>X-Tudo</h1>
           </div>
         </div>
       </div>
