ad1d1030342c039144fd0d578042618e5915c6bf
Maria de Fátima <mnunesalves334@gmail.com>
2023-11-13 12:46:12 -0300

    Ajustes no arquivo App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import AppRoutes from './Routes';
 
 function App() {
-
   return (
-      <div className="App">
-        <AppRoutes/>
-      </div>
-  )
+    <div className="App">
+      <AppRoutes />
+    </div>
+  );
 }
 
 export default App;
