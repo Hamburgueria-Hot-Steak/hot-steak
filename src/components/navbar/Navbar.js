8033e0dd453fc0ad86d01d5c4fc0f3dcfe035cdd
Maria de Fátima <mnunesalves334@gmail.com>
2023-11-17 16:31:45 -0300

    Ajustes do alinhamento dos links

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,7 +3,7 @@ import './Navbar.css';
 
 const Navbar = () => {
     return (
-      <nav className="menu-container" style={{ justifyContent: "flex-end" }}>
+      <nav className="menu-container" style={{ justifyContent: "center" }}>
           <a href="./Home"><img className="logotipo-hot-steak-horizontal" src="./assets/Logotipo Hot Steak.png" alt="Logotipo Hot Steak Horizontal" /></a>
           <a href="./Home">Home</a>
           <a href="#">Pedidos</a>
