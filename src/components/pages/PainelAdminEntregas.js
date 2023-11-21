511e944181bf3ff59fe98157b0b0c15b233c3568
Maria de Fátima <mnunesalves334@gmail.com>
2023-11-21 16:44:18 -0300

    Ajustes na tela do Painel do Admin na parte de entregas

--- src/components/pages/PainelAdminEntregas.js
+++ src/components/pages/PainelAdminEntregas.js
@@ -1,12 +1,98 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import NavbarAdmin from '../navbar/NavbarAdmin';
 import './PainelAdminEntregas.css';
 
 function PainelAdminEntregas() {
+  
+  const [popup, setPopup] = useState(null);
+  const sectionRefs = useRef([]);
+
+  useEffect(() => {
+    // Populando a ref com as seções
+    document.querySelectorAll('.section-entregas').forEach((section, index) => {
+      sectionRefs.current[index] = section;
+    });
+  }, []);
+
+  useEffect(() => {
+    // Populando a ref com as seções
+    document.querySelectorAll('.section-entregas-pendentes').forEach((section, index) => {
+      sectionRefs.current[index] = section;
+    });
+  }, []);
+
+  const nomes = [
+    'Maria de Fátima',
+    'Igor Laranjeiras',
+    'Ivanildo Marques',
+    'Gabriel Ávila',
+    'Ana Laura Acioly',
+    'João Pedro Moura',
+  ];
+
+  const enderecos = [
+    'Rua A - 1234 - Bairro A',
+    'Rua B - 5445 - Bairro B',
+    'Rua C - 6556 - Bairro C',
+    'Rua D - 7655 - Bairro D',
+    'Rua E - 9778 - Bairro E',
+    'Rua F - 9767 - Bairro F',
+  ];
+
+  // Função para exibir o aviso pop-up
+  function exibirAviso(nome, pedido, endereco) {
+    // Se já houver uma pop-up aberta, fecha a anterior
+    if (popup) {
+      popup.close();
+    }
+
+    // Cria uma nova janela pop-up
+    const novaPopup = window.open('', 'PedidoPopup', 'width=400,height=300');
+    setPopup(novaPopup);
+
+    // Adiciona o conteúdo à nova janela pop-up
+    novaPopup.document.body.innerHTML = `
+      <div>
+        <p>Nome do Cliente: ${nome}</p>
+        <p>Pedido: ${pedido}</p>
+        <p>Endereço: ${endereco}</p>
+        <button id="fecharPopupBtn">OK</button>
+      </div>
+    `;
+
+    // Adiciona um evento para fechar a pop-up quando o botão é clicado
+    const fecharBtn = novaPopup.document.getElementById('fecharPopupBtn');
+    if (fecharBtn) {
+      fecharBtn.addEventListener('click', () => {
+        novaPopup.close();
+        setPopup(null);
+      });
+    }
+  }
+
+  // Adicionando event listeners para cada seção usando refs
+  useEffect(() => {
+    sectionRefs.current.forEach((section, index) => {
+      if (section) {
+        section.addEventListener('click', () => {
+          // Obtendo as informações da seção clicada
+          const nomeCliente = nomes[index];
+          const numeroPedido = `Pedido ${index + 1}`;
+          const enderecoCliente = enderecos[index];
+
+          // Chamando a função de manipulação do clique
+          exibirAviso(nomeCliente, numeroPedido, enderecoCliente);
+        });
+      }
+    });
+  }, [sectionRefs.current, nomes, enderecos]);
+
+
     return (
         <>
            <div>
       <NavbarAdmin />
+
       <h1 className='txt-comandas'>ENTREGAS</h1>
 
       <div className='pedidos-container'>
@@ -54,23 +140,26 @@ function PainelAdminEntregas() {
           <section className='section-entregas-pendentes'>
             <img className="img-pedido7" src="./assets/opcao-hamburguer-mega.webp" alt="Pedido 7" />
             <p className='pedidos'>PEDIDO 7</p>
+            <img className="icone-entrega-pendente" src="./assets/icone-cinza.png" alt="Ícone Cinza" />
           </section>
 
           <section className='section-entregas-pendentes'>
             <img className="img-pedido8" src="./assets/opcao-hamburguer-mega.webp" alt="Pedido 8" />
             <p className='pedidos'>PEDIDO 8</p>
+            <img className="icone-entrega-pendente" src="./assets/icone-cinza.png" alt="Ícone Cinza" />
           </section>
 
           <section className='section-entregas-pendentes'>
             <img className="img-pedido9" src="./assets/opcao-hamburguer-mega.webp" alt="Pedido 9" />
             <p className='pedidos'>PEDIDO 9</p>
+            <img className="icone-entrega-pendente" src="./assets/icone-cinza.png" alt="Ícone Cinza" />
           </section>
         </div>
       </div>
     </div>
             
         </>
-    );
-}
+      );
+    }
 
 export default PainelAdminEntregas;
